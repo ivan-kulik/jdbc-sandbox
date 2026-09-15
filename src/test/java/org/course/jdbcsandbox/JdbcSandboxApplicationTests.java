12f7f2b5e9799eb79ac869785ff1e6e475c3d8f7
Ivan Kulik <ivankulik1978@gmail.com>
2026-09-15 18:52:16 +0300

    initial commit

---
+++ src/test/java/org/course/jdbcsandbox/JdbcSandboxApplicationTests.java
@@ -0,0 +1,13 @@
+package org.course.jdbcsandbox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JdbcSandboxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
